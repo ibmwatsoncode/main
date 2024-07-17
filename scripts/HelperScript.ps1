a79f8237481a07930bb3db16c60a30f66a2315a9
“ambrish-singh” <ambrish.singh@ibm.com>
2024-07-17 10:47:12 +0530

    Added power shell file

---
+++ scripts/HelperScript.ps1
@@ -0,0 +1,30 @@
+$filePath ='..\src\components'
+
+
+function Get-WatsonCodeHttp {
+  
+param (
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$WatsonPromtFirst
+)
+    Write-Output $WatsonPromtFirst #$WatsonPromtSecond inputFileContent
+}
+
+function Get-AllFile {
+            Write-Host "Get the all file name"
+            $all_file =Get-ChildItem -Path $filePath -Force
+
+            Write-Output "List of file path" @$all_file
+
+            $all_file | ForEach-Object -Parallel {
+            $content =Get-Content "$_"
+            Write-Output "Output: " $content
+            Get-WatsonCodeHttp "Explain the below code"
+}
+    
+}
+
+
+
+
+Get-AllFile
