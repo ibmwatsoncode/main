8d1beeecaaba557706e111b692853e8a86a23236
“ambrish-singh” <ambrish.singh@ibm.com>
2024-07-19 11:01:24 +0530

    path2

--- runner.ps1
+++ runner.ps1
@@ -1,14 +1,14 @@
-$filePath ='./src/components'
+#$filePath ='./src/components'
 
 function Get-AllFile {
             Write-Host "Get the all file name"
-            $all_file =Get-ChildItem -Path $filePath -Force
+            #$all_file =Get-ChildItem -Path $filePath -Force
 
-            Write-Output "List of file path" @$all_file
+           # Write-Output "List of file path" @$all_file
 
-            $all_file | ForEach-Object -Parallel {
-            $content =Get-Content "$_"
-            Write-Output "Output: " $content
+            #$all_file | ForEach-Object -Parallel {
+            #$content =Get-Content "$_"
+           # Write-Output "Output: " $content
 }
     
 }
